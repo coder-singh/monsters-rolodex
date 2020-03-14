5bc5d929a29b290cf8b674b168a652120cd064d6
Satyendra <Satyendra199612@gmail.com>
2020-03-14 21:02:07 +0530

    Updates

---
+++ precache-manifest.4a8a417f8481292172b6646a3936f432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f612bd508b1e313d48d4fae54fe62447",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e9ba20403e900fa0fa00",
+    "url": "/monsters-rolodex/static/css/main.4b0e8b64.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9ba20403e900fa0fa00",
+    "url": "/monsters-rolodex/static/js/main.e4570968.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
